f45ede70e01e6b2ca1282d32f2aa3ad9cbdd7b83
Reo Yamanaka <ryamanaka807@gmail.com>
2021-03-07 21:07:51 -0500

    made pug templates

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+
+
+// middleware/routing
+
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on ${PORT}`);
+});
+
