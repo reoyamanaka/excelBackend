5c4018d2ca54038207afced68ee559837020099f
Reo Yamanaka <ryamanaka807@gmail.com>
2021-03-07 21:20:12 -0500

    defined routes

--- index.js
+++ index.js
@@ -5,9 +5,30 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
-
 // middleware/routing
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res)=>{
+    return res.redirect('/form-with-get');
+});
+
+app.get('/form-with-get', (req, res) => {
+    return res.render('form-with-get');
+});
+
+app.get('/form-with-post', (req, res) => {
+    return res.render('form-with-post');
+});
+
+app.get('/submit-form-with-get', (req, res) => {
+    return res.send(req.query);
+});
+
+app.post('/submit-form-with-post', (req, res) => {
+    return res.send(req.body);
+});
+
 
 
 app.listen(PORT, ()=>{
