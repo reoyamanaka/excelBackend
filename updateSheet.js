27768b524aa097b509ff5671455b883d43c0cd5e
Reo Yamanaka <ryamanaka807@gmail.com>
2021-03-07 23:34:23 -0500

    updates net

--- updateSheet.js
+++ updateSheet.js
@@ -2,16 +2,28 @@ const xlsx = require('xlsx');
 const sheet = xlsx.readFile('./netCalculation.xlsx', {cellDates: true});
 const input = sheet.Sheets['input'];
 const output = sheet.Sheets['output'];
+// const xlsx_calc = require('xlsx-calc');
+// const formulajs = require('@formulajs/formulajs');
 
 function updateSheet(newRevenue, newCost) {
     var data = xlsx.utils.sheet_to_json(input);
     // console.log(data);
+    var d = xlsx.utils.sheet_to_json(output);
+
+    // console.log(data);
+    // console.log(d);
+
+    d.push({net: parseInt(newRevenue) - parseInt(newCost)});
     data.push({revenue: parseInt(newRevenue), cost: parseInt(newCost)});
     var newSheet = xlsx.utils.book_new();
-    const newContent = xlsx.utils.json_to_sheet(data);
+    var newContent = xlsx.utils.json_to_sheet(data);
+    d.length = data.length;
+    var n = xlsx.utils.json_to_sheet(d);
     xlsx.utils.book_append_sheet(newSheet, newContent, "input");
-    xlsx.utils.book_append_sheet(newSheet, output, "output");
+    xlsx.utils.book_append_sheet(newSheet, n, "output");
     xlsx.writeFile(newSheet, "newDataFile.xlsx");
+    const resultSheet = xlsx.readFile('./newDataFile.xlsx', {cellDates: true});
+
 }
 
 module.exports = updateSheet;
